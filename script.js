841c37c55f066d3f2bb9d1b62a46d5e4470ee616
AlePereiras <128554136+AlePereiras@users.noreply.github.com>
2023-06-10 12:34:15 -0300

    MANDANDO 2

--- script.js
+++ script.js
@@ -1,22 +1,36 @@
-const cardElement = document.getElementById('userCard');
-const profileImageElement = document.getElementById('profileImage');
-const nameElement = document.getElementById('name');
-const emailElement = document.getElementById('email');
-const phoneElement = document.getElementById('phone');
-const cityElement = document.getElementById('city');
-const countryElement = document.getElementById('country');
+const tableBodyElement = document.querySelector('#userTable tbody');
 
-fetch('https://randomuser.me/api/?nat=BR')
+fetch('https://randomuser.me/api/?results=10&nat=BR')
   .then(response => response.json())
   .then(data => {
-    const user = data.results[0];
-    profileImageElement.src = user.picture.large;
-    nameElement.textContent = user.name.title + ' ' + user.name.first;
-    emailElement.textContent = 'Email: ' + user.email;
-    phoneElement.textContent = 'Telefone: ' + user.phone;
-    cityElement.textContent = 'Cidade: ' + user.location.city;
-    countryElement.textContent = 'País: ' + user.location.country;
+    const users = data.results;
+    users.forEach(user => {
+      const row = document.createElement('tr');
+      const photoCell = document.createElement('td');
+      const nameCell = document.createElement('td');
+      const emailCell = document.createElement('td');
+      const phoneCell = document.createElement('td');
+      const cityCell = document.createElement('td');
+      const countryCell = document.createElement('td');
+
+      const photoImage = document.createElement('img');
+      photoImage.src = user.picture.medium;
+      photoCell.appendChild(photoImage);
+
+      nameCell.textContent = `${user.name.first} ${user.name.last}`;
+      emailCell.textContent = user.email;
+      phoneCell.textContent = user.phone;
+      cityCell.textContent = user.location.city;
+      countryCell.textContent = user.location.country;
+
+      row.appendChild(photoCell);
+      row.appendChild(nameCell);
+      row.appendChild(emailCell);
+      row.appendChild(phoneCell);
+      row.appendChild(cityCell);
+      row.appendChild(countryCell);
+
+      tableBodyElement.appendChild(row);
+    });
   })
-  .catch(error => {
-    console.log('Ocorreu um erro:', error);
-  });
+
