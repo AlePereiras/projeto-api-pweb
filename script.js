304a98944119e063b17d8e2cdf65c19afc05a6b6
AlePereiras <128554136+AlePereiras@users.noreply.github.com>
2023-06-25 13:36:39 -0300

    mandando

--- script.js
+++ script.js
@@ -1,36 +1,51 @@
 const tableBodyElement = document.querySelector('#userTable tbody');
+const paginationButtons = document.querySelectorAll('#pagination button');
 
-fetch('https://randomuser.me/api/?results=10&nat=BR')
-  .then(response => response.json())
-  .then(data => {
-    const users = data.results;
-    users.forEach(user => {
-      const row = document.createElement('tr');
-      const photoCell = document.createElement('td');
-      const nameCell = document.createElement('td');
-      const emailCell = document.createElement('td');
-      const phoneCell = document.createElement('td');
-      const cityCell = document.createElement('td');
-      const countryCell = document.createElement('td');
+function loadPage(page) {
+  const seed = 'abc';
+  const url = `https://randomuser.me/api/?results=10&nat=BR&seed=seed&page=${page}`;
 
-      const photoImage = document.createElement('img');
-      photoImage.src = user.picture.medium;
-      photoCell.appendChild(photoImage);
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const users = data.results;
+      tableBodyElement.innerHTML = '';
 
-      nameCell.textContent = `${user.name.first} ${user.name.last}`;
-      emailCell.textContent = user.email;
-      phoneCell.textContent = user.phone;
-      cityCell.textContent = user.location.city;
-      countryCell.textContent = user.location.country;
+      users.forEach(user => {
+        const row = document.createElement('tr');
+        const photoCell = document.createElement('td');
+        const nameCell = document.createElement('td');
+        const emailCell = document.createElement('td');
+        const phoneCell = document.createElement('td');
+        const cityCell = document.createElement('td');
+        const countryCell = document.createElement('td');
 
-      row.appendChild(photoCell);
-      row.appendChild(nameCell);
-      row.appendChild(emailCell);
-      row.appendChild(phoneCell);
-      row.appendChild(cityCell);
-      row.appendChild(countryCell);
+        const photoImage = document.createElement('img');
+        photoImage.src = user.picture.medium;
+        photoCell.appendChild(photoImage);
 
-      tableBodyElement.appendChild(row);
+        nameCell.textContent = `${user.name.first} ${user.name.last}`;
+        emailCell.textContent = user.email;
+        phoneCell.textContent = user.phone;
+        cityCell.textContent = user.location.city;
+        countryCell.textContent = user.location.country;
+
+        row.appendChild(photoCell);
+        row.appendChild(nameCell);
+        row.appendChild(emailCell);
+        row.appendChild(phoneCell);
+        row.appendChild(cityCell);
+        row.appendChild(countryCell);
+
+        tableBodyElement.appendChild(row);
+      });
     });
-  })
+}
 
+loadPage(1);
+
+paginationButtons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    loadPage(index + 1);
+  });
+});
